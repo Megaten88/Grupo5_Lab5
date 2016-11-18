0f9474e745b0dc30af6faa9c0bb13c05a7daa260
Carlos Guillermo Casco Valle <k_k_ryuten@live.com>
2016-11-18 14:01:21 -0600

    asdasd

--- Classmate.cpp
+++ Classmate.cpp
@@ -1,7 +1,11 @@
 #include "Classmate.h"
 using std::string;
+/*
+
+Friend::Friend(string nombre,string telef, string correo, int num):Contact(nombre,telef,correo){
+*/
+Classmate::Classmate(string nombre,string telef, string correo, int num) : Contact(nombre,telef,correo){
 
 
-Classmate::Classmate(){
 
 }
