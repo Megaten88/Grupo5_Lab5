ccabab614c1f42878c0c1e80e9be279e0ddd78c8
Mayra Salazar <mayrasalazar@unitec.edu>
2016-11-18 15:18:42 -0600

    Comenzando main

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 #include "WorkPartner.h"
 #include "Blocked.h"
 #include <iostream>
+#include <vector>
 
 int main(int argc, char const *argv[]) {
+	vector<Contact> contactos;
     std::cout << "hola" << std::endl;
     return 0;
 }
