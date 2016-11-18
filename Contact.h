17f63411e7ea82f440d5184282d5a1e926179586
Mayra Salazar <mayrasalazar@unitec.edu>
2016-11-18 13:25:54 -0600

    Comenzando Proyecto

--- Contact.h
+++ Contact.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <string>
 class Contact{
 private:
-	
+	string name;
+	string tel;
+	string email;
 public:
-
+	void setName(string);
+	string getName();
+	void setTel();
+	string getTel();
+	void setEmail();
+	string getEmail();
 };
